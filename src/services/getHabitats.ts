229d9fc1ce9d4b7ef0b5227c2bcbbd4e3cb310dc
Matheus Arauna <matheusararuna90@gmail.com>
2021-04-25 16:49:15 -0300

    refactor(Services): Delete files unused

--- src/services/getHabitats.ts
+++
@@ -1,8 +0,0 @@
-import PokeAPI from 'pokeapi-typescript'
-
-const getPokemons = () => {
-  return PokeAPI.PokemonHabitat.listAll()
-    .then((res) => console.log(res))
-    .catch((err) => err)
-}
-export default getPokemons
--- src/services/getTypes.ts
+++
@@ -1,8 +0,0 @@
-import PokeAPI from 'pokeapi-typescript'
-
-const getTypes = () => {
-  return PokeAPI.Type.listAll()
-    .then((res) => console.log(res))
-    .catch((err) => err)
-}
-export default getTypes
