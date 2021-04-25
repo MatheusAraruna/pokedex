d1faadad33f9f99cd85c428bc678c97d02f899b5
Matheus Arauna <matheusararuna90@gmail.com>
2021-04-25 00:40:24 -0300

    chore(Style): Add style grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
-import Main from 'screens/Main'
+import Filters from 'templates/Filters'
+import Pokemon from 'templates/Pokemon'
 
-const Container = styled.div`
+export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  display: grid;
+  grid-template-columns: minmax(0, 300px) 1fr;
+  ${media.lessThan('medium')`
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(2, minmax(0, auto));
+  `}
 `
 
 export default function Home() {
   return (
     <Container>
-      <Main />
+      <Filters />
+      <Pokemon />
     </Container>
   )
 }
